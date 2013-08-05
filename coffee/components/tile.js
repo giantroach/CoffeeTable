5863bb00b67321c6b2727b01dd9e67b30cb20bed
Moto <tomoki.motohashi@gmail.com>
2013-08-05 18:01:03 +0900

    tile is now simply following cards.

--- coffee/components/tile.js
+++ coffee/components/tile.js
@@ -21,59 +21,6 @@ coffee.include("Tile", "tile.html", ["Component", "Card"], function (name, ext) 
 
     // Collection
     c[name] = ext.c.Card.extend({
-    }, {
-        //~ /**
-         //~ * Add Random tile to specified group
-         //~ * @method addRandom
-         //~ * @param {String} grp
-         //~ * @return {this}
-         //~ */
-        //~ addRandom: function (grp) {
-            //~ var name = this.prototype.name,
-                //~ m = new ext.m[name],
-                //~ templates = this.def.templates[grp],
-                //~ rand = _.random(0, templates.length - 1);
-
-            //~ m.set(_.extend({
-                //~ grp: grp,
-                //~ text: "",
-                //~ draggable: true,
-                //~ rotate: 0
-            //~ }, templates[rand]));
-
-            //~ m.sendSav();
-
-            //~ return this;
-        //~ },
-
-        //~ /**
-         //~ * Add specified tile to specified group
-         //~ * @method addThis
-         //~ * @param {String} grp
-         //~ * @param {Object} args
-         //~ * <ul>
-         //~ * <li>cls: class to adding the tile.</li>
-         //~ * </ul>
-         //~ * @return {this}
-         //~ */
-        //~ addThis: function (grp, args) {
-            //~ var name = this.prototype.name,
-                //~ m = new ext.m[name],
-                //~ template = _.find(this.def.templates[grp], function (obj) {
-                    //~ return obj.cls === args.cls;
-                //~ });
-
-            //~ m.set(_.extend({
-                //~ grp: grp,
-                //~ text: "",
-                //~ draggable: true,
-                //~ rotate: 0
-            //~ }, template));
-
-            //~ m.sendSav();
-
-            //~ return this;
-        //~ }
     });
 
     // View
