55de073f56fc3f5372d0b076f13c1f92f3c89505
Moto <tomoki.motohashi@gmail.com>
2013-08-07 00:24:19 +0900

    piece is added.

---
+++ coffee/components/piece.js
@@ -0,0 +1,38 @@
+/*jslint browser: true, nomen: true, indent: 4 */
+/*global coffee */
+
+coffee.include("Piece", "piece.html", ["Component", "Card"], function (name, ext) {
+    "use strict";
+
+    var $ = ext.$,
+        Backbone = ext.Backbone,
+
+        m = {},
+        c = {},
+        r = {},
+        v = {};
+
+
+    /**
+     * Model
+     */
+    m[name] = ext.m.Card.extend({
+    });
+
+    // Collection
+    c[name] = ext.c.Card.extend({
+    });
+
+    // View
+    v[name] = ext.v.Card.extend({
+    });
+
+
+    return {
+        m : m[name],
+        c : c[name],
+        r : r[name],
+        v : v[name]
+    };
+
+});
